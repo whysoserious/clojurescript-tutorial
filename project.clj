942f634192ef2e1f178a33945de51776660ab495
Jan <janek.ziniewicz@gmail.com>
2014-08-18 12:32:23 +0200

    Initial commit

--- project.clj
+++ project.clj
@@ -3,4 +3,26 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+
+  ;; CLJ AND CLJS source code path
+  :source-paths ["src/clj" "src/cljs"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2311"]]
+
+  ;; lein-cljsbuild plugin to build a CLJS project
+  :plugins [[lein-cljsbuild "1.0.3"]]
+
+  ;; cljsbuild options configuration
+  :cljsbuild {:builds
+              [{;; CLJS source code path
+                :source-paths ["src/cljs"]
+
+                ;; Google Closure (CLS) options configuration
+                :compiler {;; CLS generated JS script filename
+                           :output-to "resources/public/js/modern.js"
+
+                           ;; minimal JS optimization directive
+                           :optimizations :whitespace
+
+                           ;; generated JS code prettyfication
+                           :pretty-print true}}]})
