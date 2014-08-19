fdb4321d02a22b6be6f19430bfd243828fff7e81
Jan <janek.ziniewicz@gmail.com>
2014-08-19 22:58:28 +0200

    modern javascript

--- resources/public/js/modern.js
+++ resources/public/js/modern.js
@@ -34804,6 +34804,34 @@ goog.net.xpc.NativeMessagingTransport.parseTransportPayload_ = function(payload)
   transportParts[1] = transportParts[1] || null;
   return transportParts;
 };
+goog.provide("modern_cljs.login");
+goog.require("cljs.core");
+modern_cljs.login.validate_form = function validate_form() {
+  var email = document.getElementById("email");
+  var password = document.getElementById("password");
+  if (cljs.core.count.call(null, email.value) > 0 && cljs.core.count.call(null, password.value) > 0) {
+    return true;
+  } else {
+    alert("Pleasem complete the form!");
+    return false;
+  }
+};
+modern_cljs.login.init = function init() {
+  if (cljs.core.truth_(function() {
+    var and__3541__auto__ = document;
+    if (cljs.core.truth_(and__3541__auto__)) {
+      return document.getElementById;
+    } else {
+      return and__3541__auto__;
+    }
+  }())) {
+    var login_form = document.getElementById("loginForm");
+    return login_form.onsubmit = modern_cljs.login.validate_form;
+  } else {
+    return null;
+  }
+};
+window.onload = modern_cljs.login.init;
 goog.provide("goog.net.xpc.FrameElementMethodTransport");
 goog.require("goog.log");
 goog.require("goog.net.xpc");
